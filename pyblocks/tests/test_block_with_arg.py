ab6cf40a80b8b909a021f7a5b197d2c3bdfef2e2
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-03-13 18:41:22 -0400

    split test files

--- pyblocks/tests/test_block_with_arg.py
+++ pyblocks/tests/test_block_with_arg.py
@@ -1,14 +1,5 @@
 #coding: pyblocks
 
-
-def block_taker(block):
-    return block()
-# Test simplest block
-x = block_taker() do:
-    return 5
-assert x == 5
-
-
 def block_taker_with_arg(x, block):
     return x + block()
 # Test block with indentation inside
---
+++ pyblocks/tests/test_simple_block.py
@@ -0,0 +1,11 @@
+#coding: pyblocks
+
+def block_taker(block):
+    return block()
+# Test simplest block
+x = block_taker() do:
+    return 5
+assert x == 5
+
+print 'OK!'
+
