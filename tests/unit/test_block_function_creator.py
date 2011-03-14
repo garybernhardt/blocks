30fbf8656654a31f7fc143980192071a81f42ba7
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-03-14 16:35:38 -0400

    remove dedent call duplication

--- tests/unit/test_block_function_creator.py
+++ tests/unit/test_block_function_creator.py
@@ -6,29 +6,29 @@ from blocks.blockfunctioncreator import BlockFunctionCreator
 class describe_block_function_creator:
     def it_translates_blocks_to_functions(self):
         assert_translated(
-            dedent("""
-                   describe() do:
-                       return 5
-                   """),
-            dedent("""
-                   describe (%(function_name)s )
-                   def %(function_name)s ():
+            """
+            describe() do:
+                return 5
+            """,
+            """
+            describe (%(function_name)s )
+            def %(function_name)s ():
 
-                       return 5 
-                   """))
+                return 5 
+            """)
 
     def it_passes_block_function_arguments(self):
         assert_translated(
-            dedent("""
-                   describe(4) do:
-                       return 5
-                   """),
-            dedent("""
-                   describe (4 ,%(function_name)s )
-                   def %(function_name)s ():
+            """
+            describe(4) do:
+                return 5
+            """,
+            """
+            describe (4 ,%(function_name)s )
+            def %(function_name)s ():
 
-                       return 5 
-                   """))
+                return 5 
+            """)
 
     # def it_translates_nested_blocks
     # def it_translates_with_code_before_and_after_block_calls
@@ -36,6 +36,7 @@ class describe_block_function_creator:
 
 
 def assert_translated(original, expected):
+    original, expected = dedent(original), dedent(expected)
     creator = BlockFunctionCreator(original)
     translated = creator.translate()
     function_name = creator.function_name
