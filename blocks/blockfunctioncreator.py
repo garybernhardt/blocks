1e53475458815a7e524773aa0868ea962ed2cee3
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-03-14 16:28:24 -0400

    preserve args when calling block takers

--- blocks/blockfunctioncreator.py
+++ blocks/blockfunctioncreator.py
@@ -1,16 +1,90 @@
 from textwrap import dedent
 import random
+from tokenize import generate_tokens, NAME, OP, STRING, NL, INDENT, DEDENT
+from StringIO import StringIO
+import tokenize
 
 
 class BlockFunctionCreator:
     def __init__(self, source):
+        self.source = source
         self.function_name = ''.join(random.choice('0123456789')
                                      for _ in range(16))
 
     def translate(self):
-        return dedent("""
-                      describe(%s)
-                      def %s():
-                      return 5
-                      """ % (self.function_name, self.function_name))
+        tokens = generate_tokens(StringIO(self.source).readline)
+
+        result = []
+
+        for tokenum, value, _, _, _ in tokens:
+            if tokenum == NAME and value == 'do':
+                BlockTranslator(tokens, result, self.function_name).translate()
+            else:
+                result.append([tokenum, value])
+
+        return tokenize.untokenize(result)
+
+
+class BlockTranslator:
+    def __init__(self, token_generator, result, function_name):
+        self.token_generator = token_generator
+        self.result = result
+        self.function_name = function_name
+
+    def anonymous_function_name(self):
+        random_string = ''.join(random.choice('0123456789')
+                                for _ in range(32))
+        return '_anon_func_%s' % random_string
+
+    def translate(self):
+        # Consume the colon after the "do"
+        self.token_generator.next()
+
+        # Add the new anonymous function as an argument to the call
+        self.add_anonymous_function_as_argument()
+
+        # Add the function definition
+        self.add_anonymous_function()
+
+        # Fast forward past the body of the function
+        self.fast_forward_to_end_of_block_definition()
+
+    def add_anonymous_function(self):
+        self.result.append([NAME, 'def'])
+        self.result.append([NAME, self.function_name])
+        self.result.append([OP, '('])
+        self.result.append([OP, ')'])
+        self.result.append([OP, ':'])
+        self.result.append([NL, '\n'])
+
+    def fast_forward_to_end_of_block_definition(self):
+        """
+        Read tokens until we're at the indentation level higher that we started
+        at, which means we've exited the block definition
+        """
+        indentation_level = 0
+        seen_indent = False
+
+        while True:
+            tokenum, value, _, _, _ = self.token_generator.next()
+            self.result.append([tokenum, value])
+
+            if tokenum == INDENT:
+                indentation_level += 1
+                seen_indent = True
+            elif tokenum == DEDENT:
+                indentation_level -= 1
+
+            if seen_indent and indentation_level == 0:
+                break
+
+    def add_anonymous_function_as_argument(self):
+        closing_function_call_paren = self.result.pop()
+        last_token_in_arg_list = self.result[-1]
+        call_has_other_args = last_token_in_arg_list != [OP, '(']
+        if call_has_other_args:
+            self.result.append([OP, ','])
+        self.result.append([NAME, self.function_name])
+        self.result.append(closing_function_call_paren)
+        self.result.append([NL, '\n'])
 
--- tests/unit/test_block_function_creator.py
+++ tests/unit/test_block_function_creator.py
@@ -13,9 +13,10 @@ class describe_block_function_creator:
         translated = creator.translate()
         function_name = creator.function_name
         expected = dedent("""
-                          describe(%s)
-                          def %s():
-                          return 5
+                          describe (%s )
+                          def %s ():
+
+                              return 5 
                           """ %
                           (function_name, function_name))
         expect(translated) == expected
@@ -24,4 +25,21 @@ class describe_block_function_creator:
         creator1 = BlockFunctionCreator('')
         creator2 = BlockFunctionCreator('')
         expect(creator1.function_name) != creator2.function_name
+
+    def it_passes_block_function_arguments(self):
+        source = dedent("""
+                        describe(4) do:
+                            return 5
+                        """)
+        creator = BlockFunctionCreator(source)
+        translated = creator.translate()
+        function_name = creator.function_name
+        expected = dedent("""
+                          describe (4 ,%s )
+                          def %s ():
+
+                              return 5 
+                          """ %
+                          (function_name, function_name))
+        expect(translated) == expected
 
