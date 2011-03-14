cd126a7bc07bdca5b49dca3ce94908326036ddb7
Gary Bernhardt <gary.bernhardt@gmail.com>
2011-03-14 16:30:58 -0400

    extract test duplication to assert_translated

--- tests/unit/test_block_function_creator.py
+++ tests/unit/test_block_function_creator.py
@@ -5,21 +5,17 @@ from blocks.blockfunctioncreator import BlockFunctionCreator
 
 class describe_block_function_creator:
     def it_translates_blocks_to_functions(self):
-        source = dedent("""
-                        describe() do:
-                            return 5
-                        """)
-        creator = BlockFunctionCreator(source)
-        translated = creator.translate()
-        function_name = creator.function_name
-        expected = dedent("""
-                          describe (%s )
-                          def %s ():
+        assert_translated(
+            dedent("""
+                   describe() do:
+                       return 5
+                   """),
+            dedent("""
+                   describe (%(function_name)s )
+                   def %(function_name)s ():
 
-                              return 5 
-                          """ %
-                          (function_name, function_name))
-        expect(translated) == expected
+                       return 5 
+                   """))
 
     def it_generates_unique_function_names(self):
         creator1 = BlockFunctionCreator('')
@@ -27,23 +23,26 @@ class describe_block_function_creator:
         expect(creator1.function_name) != creator2.function_name
 
     def it_passes_block_function_arguments(self):
-        source = dedent("""
-                        describe(4) do:
-                            return 5
-                        """)
-        creator = BlockFunctionCreator(source)
-        translated = creator.translate()
-        function_name = creator.function_name
-        expected = dedent("""
-                          describe (4 ,%s )
-                          def %s ():
+        assert_translated(
+            dedent("""
+                   describe(4) do:
+                       return 5
+                   """),
+            dedent("""
+                   describe (4 ,%(function_name)s )
+                   def %(function_name)s ():
 
-                              return 5 
-                          """ %
-                          (function_name, function_name))
-        expect(translated) == expected
+                       return 5 
+                   """))
 
     # def it_translates_nested_blocks
     # def it_translates_with_code_before_and_after_block_calls
     # def it_translates_blocks_with_arguments
+
+
+def assert_translated(original, expected):
+    creator = BlockFunctionCreator(original)
+    translated = creator.translate()
+    function_name = creator.function_name
+    expect(translated) == expected % dict(function_name = function_name)
 
